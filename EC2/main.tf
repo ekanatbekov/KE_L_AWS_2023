dfd15316dfe4c99a8c4f737d7557a865e6bd5993
Esen <79772116+ekanatbekov@users.noreply.github.com>
2023-02-24 20:19:06 -0600

    instance type

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ke_instance" {
   ami                         = ""
-  instance_type               = ""
+  instance_type               = "t2.nano"
   count                       = 1
   availability_zone           = "us-east-1"
   associate_public_ip_address = true
